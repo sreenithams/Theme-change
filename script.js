90c07b898fc741acb6d051d399c2cc0adb3fae89
sreenithams <sreenithams05@gmail.com>
2024-10-16 14:53:23 +0530

    finalcommit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btn = document.querySelector("#btn");
 btn.addEventListener("click",() => {
-    console.log("active")
     if (btn.innerHTML === "Dark") {
         btn.innerHTML="white"
          document.body.style.backgroundColor="black"
